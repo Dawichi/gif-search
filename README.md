061e6222d0eda2c2e043039844862d1ea8e0396c
David M. Fajardo <davidmf99@gmail.com>
2021-07-25 13:33:04 +0200

    backgrounds

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![forthebadge](https://forthebadge.com/images/badges/made-with-typescript.svg)](https://forthebadge.com)
 
+
+Search GIFs with the Giphy API!
+
 ![showcase](https://github.com/Dawichi/gif-search/blob/main/public/showcase.png)
 
-Search GIFs with the Giphy API
--- components/GifGridItem.tsx
+++ components/GifGridItem.tsx
@@ -5,7 +5,15 @@ export const GifGridItem = ({ title, url}) => {
     
     return (
         <div className='animate__animated animate__fadeIn'>
-            <img src={ url } alt={ title } className='rounded-lg w-full h-full' />
+			<div className='relative w-full rounded-lg'
+				style={{
+					paddingTop: '100%',
+					backgroundImage: `url('${url}')`,
+					backgroundPosition: 'center',
+					backgroundSize: 'cover'
+				}}>
+			</div>
+			<span>{ title }</span>
         </div>
     )
 }
